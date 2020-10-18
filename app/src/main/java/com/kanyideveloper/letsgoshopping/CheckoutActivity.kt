1eccbd60c92294a5288614d49fadf882fea71be3
JoelKanyi <joelkanyi98@gmail.com>
2020-10-18 12:36:42 +0300

    I guess CheckoutActivity is working fine now

--- app/src/main/java/com/kanyideveloper/letsgoshopping/CheckoutActivity.kt
+++ app/src/main/java/com/kanyideveloper/letsgoshopping/CheckoutActivity.kt
@@ -10,15 +10,12 @@ import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_checkout.*
 import kotlinx.android.synthetic.main.cart_items_row.*
 
-
 class CheckoutActivity : AppCompatActivity(), ItemClickListener {
-
     private lateinit var mDatabase: FirebaseDatabase
     private lateinit var mRecyclerView: RecyclerView
     private lateinit var mReference: DatabaseReference
     private var cartList : ArrayList<CartItem>? = null
     private lateinit var adapter : CartItemsAdapter
-
     private val sharedPrefFile = "kotlinsharedpreference"
     private lateinit var sharedPreferences: SharedPreferences
 
@@ -27,12 +24,9 @@ class CheckoutActivity : AppCompatActivity(), ItemClickListener {
         setContentView(R.layout.activity_checkout)
 
         sharedPreferences = this.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
-
         mDatabase = FirebaseDatabase.getInstance()
-
-        cartList = arrayListOf()
-
         mReference = mDatabase.getReference("cart_items")
+        cartList = arrayListOf()
 
         mReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot : DataSnapshot) {
@@ -41,28 +35,26 @@ class CheckoutActivity : AppCompatActivity(), ItemClickListener {
                         val item = i.getValue(CartItem::class.java)
                         cartList!!.add(item!!)
                     }
-
                     var sum = 0.0
                     var vat = 0.0
                     var total = 0.0
                     for (itm : CartItem in cartList!!) {
-                        sum += itm.itemPrice.toDouble()
+                        sum +=  itm.itemPrice.toDouble() * itm.counter
                         vat += itm.vat.toDouble()
                         total = sum + vat
                     }
                     subtotal_value.text = sum.toString()
                     vat_value.text = vat.toString()
-
                     total_value.text = total.toString()
-
-                    //Initialise my adapter
-                    //adapter = CartItemsAdapter(applicationContext, cartList!!, this@CheckoutActivity)
-                    //mRecyclerView.adapter = adapter
 
                     initialiseRecycler()
                 }
+                else{
+                    subtotal_value.text = "0.0"
+                    vat_value.text = "0.0"
+                    total_value.text = "0.0"
+                }
             }
-
             override fun onCancelled(error : DatabaseError) {
             }
         })
@@ -73,12 +65,12 @@ class CheckoutActivity : AppCompatActivity(), ItemClickListener {
         val mQuery : Query = mReference.orderByChild("itemName").equalTo(item.itemName)
         mQuery.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot : DataSnapshot) {
-                for (snapshot in snapshot.children) {
-                    snapshot.child("counter").ref.setValue(item.counter)
+                for (mSnapshot in snapshot.children) {
+                    mSnapshot.child("counter").ref.setValue(item.counter)
                     adapter.clear()
+                    incrementCounter()
                 }
             }
-
             override fun onCancelled(error : DatabaseError) {
             }
         })
@@ -88,35 +80,34 @@ class CheckoutActivity : AppCompatActivity(), ItemClickListener {
         val mQuery : Query = mReference.orderByChild("itemName").equalTo(item.itemName)
         mQuery.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot : DataSnapshot) {
-                for (snapshot in snapshot.children) {
-                    snapshot.child("counter").ref.setValue(item.counter)
+                for (mSnapshot in snapshot.children) {
+                    mSnapshot.child("counter").ref.setValue(item.counter)
                     adapter.clear()
+                    decrementCounter(1)
                 }
             }
-
             override fun onCancelled(error : DatabaseError) {
             }
         })
     }
-
     override fun deleteFromCart(item : CartItem, position : Int) {
-
         val mQuery : Query = mReference.orderByChild("itemName").equalTo(item.itemName)
         mQuery.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot : DataSnapshot) {
-                for (snapshot in snapshot.children) {
-                    snapshot.ref.removeValue()
-                    removeItem(position)
-                    adapter.clear()
-                    decrementCounter()
+                if (snapshot.exists()) {
+                    for (mSnapshot in snapshot.children) {
+                        val itmCount = item.counter
+                        mSnapshot.ref.removeValue()
+                        removeItem(position)
+                        adapter.clear()
+                        decrementCounter(itmCount)
+                    }
                 }
             }
-
             override fun onCancelled(error : DatabaseError) {
             }
         })
     }
-
     private fun incrementCounter(){
         val editor: SharedPreferences.Editor = sharedPreferences.edit()
         editor.putInt(Utils.counter.toString(), sharedPreferences.getInt(Utils.counter.toString(), 0) + 1)
@@ -125,21 +116,18 @@ class CheckoutActivity : AppCompatActivity(), ItemClickListener {
         val sharedIdValue = sharedPreferences.getInt(Utils.counter.toString(), 0)
         items_num.text = sharedIdValue.toString()
     }
-
-    private fun decrementCounter(){
+    private fun decrementCounter(num : Int){
         val editor: SharedPreferences.Editor = sharedPreferences.edit()
-        editor.putInt(Utils.counter.toString(), sharedPreferences.getInt(Utils.counter.toString(), 0) - 1)
+        editor.putInt(Utils.counter.toString(), sharedPreferences.getInt(Utils.counter.toString(), 0) - num)
         editor.apply()
         editor.commit()
         val sharedIdValue = sharedPreferences.getInt(Utils.counter.toString(), 0)
         items_num.text = sharedIdValue.toString()
     }
-
     private fun removeItem(position : Int){
         cartList?.removeAt(position)
         adapter.notifyItemRemoved(position)
     }
-
     private fun initialiseRecycler(){
         mRecyclerView = findViewById(R.id.cart_recycler)
         mRecyclerView.hasFixedSize()
